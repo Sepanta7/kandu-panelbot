e921c66f4b8c427ed060212b963df0ae3c5c5aec
sepanta <163876726+Sepanta7@users.noreply.github.com>
2024-10-20 21:04:27 +0330

    Update createdb.php
    Deleting the previous tables of the robot database

--- createdb.php
+++ createdb.php
@@ -1,16 +1,12 @@
 <?php
-// وارد کردن اطلاعات اتصال به دیتابیس از فایل baseinfo.php
 require_once 'baseinfo.php';
 
-// ایجاد اتصال به دیتابیس
 $conn = new mysqli($servername, $username, $password);
 
-// بررسی اتصال
 if ($conn->connect_error) {
     die("اتصال به دیتابیس با خطا مواجه شد: " . $conn->connect_error);
 }
 
-// ایجاد دیتابیس در صورتی که وجود ندارد
 $sql = "CREATE DATABASE IF NOT EXISTS $dbname";
 if ($conn->query($sql) === TRUE) {
     echo "دیتابیس با موفقیت ایجاد شد یا قبلاً وجود داشته است.<br>";
@@ -18,31 +14,41 @@ if ($conn->query($sql) === TRUE) {
     echo "خطا در ایجاد دیتابیس: " . $conn->error . "<br>";
 }
 
-// انتخاب دیتابیس
 $conn->select_db($dbname);
 
-// ایجاد جدول black_list
+$sql = "DROP TABLE IF EXISTS black_list";
+if ($conn->query($sql) === TRUE) {
+    echo "جدول black_list با موفقیت حذف شد.<br>";
+} else {
+    echo "خطا در حذف جدول black_list: " . $conn->error . "<br>";
+}
+
+$sql = "DROP TABLE IF EXISTS users";
+if ($conn->query($sql) === TRUE) {
+    echo "جدول users با موفقیت حذف شد.<br>";
+} else {
+    echo "خطا در حذف جدول users: " . $conn->error . "<br>";
+}
+
 $sql = "CREATE TABLE IF NOT EXISTS black_list (
     chatid BIGINT PRIMARY KEY
 )";
 if ($conn->query($sql) === TRUE) {
-    echo "جدول black_list با موفقیت ایجاد شد یا قبلاً وجود داشته است.<br>";
+    echo "جدول black_list با موفقیت ایجاد شد.<br>";
 } else {
     echo "خطا در ایجاد جدول black_list: " . $conn->error . "<br>";
 }
 
-// ایجاد جدول users
 $sql = "CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     balance DECIMAL(10, 2) NOT NULL,
     number VARCHAR(255) NOT NULL
 )";
 if ($conn->query($sql) === TRUE) {
-    echo "جدول users با موفقیت ایجاد شد یا قبلاً وجود داشته است.<br>";
+    echo "جدول users با موفقیت ایجاد شد.<br>";
 } else {
     echo "خطا در ایجاد جدول users: " . $conn->error . "<br>";
 }
 
-// بستن اتصال به دیتابیس
 $conn->close();
 ?>
